93b273ed421e93f4b60c363af604ecf422c7315b
Frank Ch. Eigler <fche@redhat.com>
2013-04-12 14:20:01 -0400

    PR15363: tweak registration-warning message to print inode-offset
    Sadly, virtual address to feed/decode objdump -d is not available.

--- runtime/linux/uprobes-inode.c
+++ runtime/linux/uprobes-inode.c
@@ -355,8 +355,8 @@ stapiu_target_reg(struct stapiu_target *target, struct task_struct* task)
 			ret = stapiu_register(target->inode, c);
 			if (ret) {
 				c->registered = 0;
-				_stp_warn("probe %s registration error (rc %d)",
-					   c->probe->pp, ret);
+				_stp_warn("probe %s inode-offset %p registration error (rc %d)",
+                                          c->probe->pp, (void*) c->offset, ret);
                                 ret = 0; /* Don't abort entire stap script just for this. */
 			}
 			c->registered = 1;
