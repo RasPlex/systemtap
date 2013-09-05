638f96406326aa21f895984f9e9982b993085cf6
Frank Ch. Eigler <fche@redhat.com>
2013-09-05 15:16:41 -0400

    minidebuginfo: tighten up create-minidebug.sh
    ... so we remove .debug_* and .sym* stuff, leaving only the
    new .gnu_debugdata.

--- testsuite/systemtap.base/create-minidebug.sh
+++ testsuite/systemtap.base/create-minidebug.sh
@@ -15,7 +15,11 @@ comm -13 dynsyms funcsyms > keep_symbols
 # removing some unnecessary sections.
 objcopy -S --remove-section .gdb_index --remove-section .comment --keep-symbols=keep_symbols binary mini_debuginfo
 
+# Drop the DWARF debuginfo to ensure we're only using minidebuginfo symbol table magic
+strip -s binary
+
 # Inject the compressed data into the .gnu_debugdata section of the
 # original binary.
+rm -f mini_debuginfo.xz
 xz mini_debuginfo
 objcopy --add-section .gnu_debugdata=mini_debuginfo.xz binary
