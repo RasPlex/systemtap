2b5bbe5ac90151618c72d9d61473b58893842515
Lukas Berk <lberk@redhat.com>
2013-09-06 13:41:39 -0400

    Fix minidebuginfo.exp testcase
    The minidebuginfo testcase was based off of an incomplete documenation.  We need
    to properly fix the creation of the executable to test in a standalone instance.
    
    create-minidebug.sh - properly create standalone testcase, this includes
    		      removing all symbol, relocation and .debug_* sections.
    		      Tweak naming of files with binary. prefix for easier
    		      tracking
    
    minidebuginfo.exp - updated file names to target and cleanup

--- testsuite/systemtap.base/create-minidebug.sh
+++ testsuite/systemtap.base/create-minidebug.sh
@@ -2,24 +2,31 @@
 
 # Extract the dynamic symbols from the main binary, there is no need
 # to also have these in the normal symbol table
-nm -D binary --format=posix --defined-only | awk '{ print $1 }' | sort > dynsyms
+nm -D binary --format=posix --defined-only | awk '{ print $1 }' | sort > binary.dynsyms
 
 # Extract all the text (i.e. function) symbols from the debuginfo .
-nm binary --format=posix --defined-only | awk '{ if ($2 == "T" || $2 == "t") print $1 }' | sort > funcsyms
+nm binary --format=posix --defined-only | awk '{ if ($2 == "T" || $2 == "t" || $2 == "D") print $1 }' | sort > binary.funcsyms
 
 # Keep all the function symbols not already in the dynamic symbol
 # table.
-comm -13 dynsyms funcsyms > keep_symbols
+comm -13 binary.dynsyms binary.funcsyms > binary.keep_symbols
+
+# We don't have a split executable in advance
+# so we need to strip the binary ourselves
+strip --strip-all -o binary.strip binary
+
+# Separate full debug info into debug binary.
+objcopy --only-keep-debug binary binary.debug
 
 # Copy the full debuginfo, keeping only a minimal set of symbols and
 # removing some unnecessary sections.
-objcopy -S --remove-section .gdb_index --remove-section .comment --keep-symbols=keep_symbols binary mini_debuginfo
+rm -f binary.mini_debuginfo
+objcopy -S --remove-section .gdb_index --remove-section .comment --keep-symbols=binary.keep_symbols binary.debug binary.mini_debuginfo
 
-# Drop the DWARF debuginfo to ensure we're only using minidebuginfo symbol table magic
-strip -s binary
+#compress the binary.mini_debuginfo while keeping original file
+rm -f binary.mini_debuginfo.xz
+xz -k binary.mini_debuginfo
 
 # Inject the compressed data into the .gnu_debugdata section of the
-# original binary.
-rm -f mini_debuginfo.xz
-xz mini_debuginfo
-objcopy --add-section .gnu_debugdata=mini_debuginfo.xz binary
+# original binary
+objcopy --add-section .gnu_debugdata=binary.mini_debuginfo.xz binary.strip binary.test
