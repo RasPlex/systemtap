228f84aea6a0bee28b5e095aeead7cbdd5553643
Miroslav Franc <mfranc@redhat.com>
2013-11-11 08:55:24 -0600

    Fixed a typo introduced in commit 7acb3e34.
    * runtime/linux/uprobes-common.c (stap_uprobe_change_plus): The outer if
      statement use curly braces to create a block, however the ending
      curly brace ended up outside macro conditional.

--- runtime/linux/uprobes-common.c
+++ runtime/linux/uprobes-common.c
@@ -128,11 +128,11 @@ static int stap_uprobe_change_plus (struct task_struct *tsk, unsigned long reloc
       /* NB: duplicates common_entryfn_epilogue,
 	 but then this is not a probe entry fn epilogue. */
 #ifndef STAP_SUPPRESS_HANDLER_ERRORS
-        if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
-          if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
+      if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
+        if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
           _stp_error ("Skipped too many probes, check MAXSKIPPED or try again with stap -t for more details.");
-#endif
       }
+#endif
     }
   }  /* close iteration over stap_uprobe_spec[] */
   return 0; /* XXX: or rc? */

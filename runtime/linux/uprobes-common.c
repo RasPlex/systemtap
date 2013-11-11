RasPlex/systemtap

commit 7acb3e34eedff21a2bbdc6c5b6cf77a001612445

PR15908: pass STAP_SUPPRESS_HANDLER_ERRORS to runtime
* session.cxx (parse_cmdline): Handle --suppress-handle-errors more.
* runtime/linux/uprobes-common.c: Respect new macro in MAXSKIPPED
  processing.

runtime/linux/uprobes-common.c
@@ -127,9 +127,11 @@ static int stap_uprobe_change_plus (struct task_struct *tsk, unsigned long reloc
       #endif
       /* NB: duplicates common_entryfn_epilogue,
 	 but then this is not a probe entry fn epilogue. */
-      if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
-        if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
-        _stp_error ("Skipped too many probes, check MAXSKIPPED or try again with stap -t for more details.");
+#ifndef STAP_SUPPRESS_HANDLER_ERRORS
+        if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
+          if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
+          _stp_error ("Skipped too many probes, check MAXSKIPPED or try again with stap -t for more details.");
+#endif
       }
     }
   }  /* close iteration over stap_uprobe_spec[] */

session.cxx
@@ -1150,6 +1150,7 @@ systemtap_session::parse_cmdline (int argc, char * const argv [])
 
 	case LONG_OPT_SUPPRESS_HANDLER_ERRORS:
 	  suppress_handler_errors = true;
+          c_macros.push_back (string ("STAP_SUPPRESS_HANDLER_ERRORS"));
 	  break;
 
 	case LONG_OPT_MODINFO:

commit 228f84aea6a0bee28b5e095aeead7cbdd5553643

Fixed a typo introduced in commit 7acb3e34.
* runtime/linux/uprobes-common.c (stap_uprobe_change_plus): The outer if
  statement use curly braces to create a block, however the ending
  curly brace ended up outside macro conditional.

runtime/linux/uprobes-common.c
@@ -128,11 +128,11 @@ static int stap_uprobe_change_plus (struct task_struct *tsk, unsigned long reloc
       /* NB: duplicates common_entryfn_epilogue,
 	 but then this is not a probe entry fn epilogue. */
 #ifndef STAP_SUPPRESS_HANDLER_ERRORS
-        if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
-          if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
+      if (unlikely (atomic_inc_return (skipped_count()) > MAXSKIPPED)) {
+        if (unlikely (pseudo_atomic_cmpxchg(session_state(), STAP_SESSION_RUNNING, STAP_SESSION_ERROR) == STAP_SESSION_RUNNING))
           _stp_error ("Skipped too many probes, check MAXSKIPPED or try again with stap -t for more details.");
-#endif
       }
+#endif
     }
   }  /* close iteration over stap_uprobe_spec[] */
   return 0; /* XXX: or rc? */